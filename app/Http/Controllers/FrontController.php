8d76a5f667dcf9fb5ff2f3089a813e0ca92baa12
Ilham <mi1797128@gmail.com>
2024-08-28 10:55:17 +0700

    menambahkan search layout

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreSearchHotelRequest;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index(){
         return view('front.index');
+    }
+    public function hotels(){
+        return view('front.hotels');
+    }
+    public function search_hotels(StoreSearchHotelRequest $request){
+
     }
 }
